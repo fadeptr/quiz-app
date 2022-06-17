6b60a2cbfca270dc4a2b53ac957e1ccb00606af5
aiferrydermawan <aiferrydermawan@gmail.com>
2022-06-18 00:38:12 +0700

    firsyt

---
+++ resources/views/livewire/landing-page.blade.php
@@ -0,0 +1,85 @@
+<div>
+    <div class="container">
+        <section>
+            <h1>{{ config('app.name') }}</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat ullam harum ex ea sit, ratione sequi culpa aperiam, quia dicta, itaque esse fugit exercitationem obcaecati quisquam. Tempora dolore vel minus reiciendis debitis assumenda tenetur obcaecati temporibus quod cumque consequuntur ea repellat fuga nemo, necessitatibus fugiat eum ipsa eius sunt? Beatae deserunt illum molestias dolor, alias voluptatem pariatur mollitia voluptatibus id. Illo eum ad sequi, est ipsam aliquam eos quas, sit optio quae quos, numquam enim atque. Aliquid libero pariatur totam, quisquam adipisci et. Nemo, in ipsum quam illo doloribus at numquam necessitatibus ipsam cupiditate possimus commodi, dolorem, magnam sed nesciunt.</p>
+            <a href="" class="btn btn-primary">Submit</a>
+        </section>
+        <section class="mt-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star-fill mb-4" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>
+                    <h5>Berkualitas</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt distinctio deserunt illo aperiam debitis aut vitae nulla reprehenderit exercitationem? At dicta praesentium aliquam quae corrupti.</p>
+                </div>
+                <div class="col-md-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star-fill mb-4" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>
+                    <h5>Mudah Dipahami</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt distinctio deserunt illo aperiam debitis aut vitae nulla reprehenderit exercitationem? At dicta praesentium aliquam quae corrupti.</p>
+                </div>
+                <div class="col-md-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star-fill mb-4" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>
+                    <h5>Fleksibel</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt distinctio deserunt illo aperiam debitis aut vitae nulla reprehenderit exercitationem? At dicta praesentium aliquam quae corrupti.</p>
+                </div>
+            </div>
+        </section>
+        <section class="mt-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Paket 1 Bulan</div>
+                        <div class="card-body">
+                            <h6 class="text-danger">
+                                <s>Rp.1.000.000</s>
+                            </h6>
+                            <h3>Rp.100.000</h3>
+                            <a href="" class="btn btn-primary">Beli Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Paket 1 Bulan</div>
+                        <div class="card-body">
+                            <h6 class="text-danger">
+                                <s>Rp.1.000.000</s>
+                            </h6>
+                            <h3>Rp.100.000</h3>
+                            <a href="" class="btn btn-primary">Beli Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Paket 1 Bulan</div>
+                        <div class="card-body">
+                            <h6 class="text-danger">
+                                <s>Rp.1.000.000</s>
+                            </h6>
+                            <h3>Rp.100.000</h3>
+                            <a href="" class="btn btn-primary">Beli Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="mt-5">
+            <hr>
+            <div class="d-flex justify-content-between">
+                <div>Copyright &copy; Faisal Ade Putra 2022</div>
+                <div>
+                    <a href="" class="text-primary me-3">Menu 1</a>
+                    <a href="" class="text-primary me-3">Menu 2</a>
+                    <a href="" class="text-primary">Menu 3</a>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
